24a5c06d214dacacee72762949caa34beef3599b
Anthony Ryan <tonyryan07@gmail.com>
2016-05-26 11:50:15 -0400

    Add project files.

---
+++ Filmingo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Filmingo.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Filmingo";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
